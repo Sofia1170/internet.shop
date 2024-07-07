8b41c0bb024387d789384e036b1c21b7726c5d95
Sofia1170 <sofilo2009@mail.ru>
2024-07-07 14:02:32 +0300

    создание отзывов

--- internetshop/shop/models.py
+++ internetshop/shop/models.py
@@ -6,7 +6,18 @@ class Product(models.Model):
     description = models.TextField()
     price = models.IntegerField()
     image_url = models.CharField(max_length=256)
-    brand = models.TextField(null=True, blank=True)
+    brandMark = models.CharField(max_length=128, null=True, blank=True)
+    color = models.CharField(max_length=128, null=True, blank=True)
+
+
 
     def __str__(self):
         return f'{self.name}'
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+
+    author = models.CharField(max_length=256)
+    rating = models.IntegerField()
+    text = models.TextField()
